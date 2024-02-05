93e044f590a5983d29d0da5e46a6a86280e9dd87
Lilyan <lilyan.leblanc@gmail.com>
2024-02-05 15:57:16 +0100

    VtV_029 - Compute and display errors of registration

---
+++ RegistrationError.m
@@ -0,0 +1,12 @@
+classdef RegistrationError
+    properties
+        error_X
+        error_Y
+        error_Z
+        error_R
+        error_P
+        error_Yaw
+    end
+    methods
+    end
+end
